cd92804ef3874b7f5e6145df181a4f7331b15baf
noobMisaka <1352270353@qq.com>
2024-02-20 17:28:21 +0800

    整理按键定义

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,12 @@ public class Player : MonoBehaviour
     Animator anim;
     new SpriteRenderer renderer;
     float moveSpeed = 0.05f;
+    KeyCode upKey = KeyCode.W;
+    KeyCode downKey = KeyCode.S;
+    KeyCode leftKey = KeyCode.A;
+    KeyCode rightKey = KeyCode.D;
+    KeyCode attackKey = KeyCode.J;
+
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -17,27 +23,27 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.D))
+        if(Input.GetKey(rightKey))
         {
             transform.Translate(moveSpeed, 0f, 0f);
             renderer.flipX = false;
             anim.SetBool("run", true);
             Attack();
         }
-        else if(Input.GetKey(KeyCode.A))
+        else if(Input.GetKey(leftKey))
         {
             transform.Translate(-moveSpeed, 0f, 0f);
             renderer.flipX = true;
             anim.SetBool("run", true);
             Attack();
         }
-        else if(Input.GetKey(KeyCode.W))
+        else if(Input.GetKey(upKey))
         {
             transform.Translate(0f, moveSpeed, 0f);
             anim.SetBool("run", true);
             Attack();
         }
-        else if(Input.GetKey(KeyCode.S))
+        else if(Input.GetKey(downKey))
         {
             transform.Translate(0f, -moveSpeed, 0f);
             anim.SetBool("run", true);
@@ -53,7 +59,7 @@ public class Player : MonoBehaviour
 
     void Attack()
     {
-        if(Input.GetKey(KeyCode.J))
+        if(Input.GetKey(attackKey))
         {
             anim.SetBool("attack", true);
         }
