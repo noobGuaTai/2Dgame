ad8a09ebcc69c1d9c7475284d933cab677ed60a1
noobMisaka <1352270353@qq.com>
2024-02-20 15:38:27 +0800

    添加人物待机、攻击动画和人物移动

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,57 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+    Animator anim;
+    new SpriteRenderer renderer;
+    float moveSpeed = 0.05f;
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(moveSpeed, 0f, 0f);
+            renderer.flipX = false;
+            anim.SetBool("run", true);
+            Attack();
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-moveSpeed, 0f, 0f);
+            renderer.flipX = true;
+            anim.SetBool("run", true);
+            Attack();
+        }
+        else if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0f, moveSpeed, 0f);
+            anim.SetBool("run", true);
+            Attack();
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0f, -moveSpeed, 0f);
+            anim.SetBool("run", true);
+            Attack();
+        }
+        else
+        {
+            anim.SetBool("run", false);
+            anim.SetBool("attack", false);
+        }
+        Attack();
+    }
+
+    void Attack()
+    {
+        if(Input.GetKey(KeyCode.J))
+        {
+            anim.SetBool("attack", true);
+        }
     }
 }
