5f6bd7bafd47e439f35b9d9079e3268ba565adcb
noobMisaka <1352270353@qq.com>
2024-02-21 20:13:01 +0800

    重写角色移动攻击，添加跳跃

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,54 +8,61 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     Animator anim;
     new SpriteRenderer renderer;
-    float moveSpeed = 0.05f;
-    KeyCode upKey = KeyCode.W;
-    KeyCode downKey = KeyCode.S;
-    KeyCode leftKey = KeyCode.A;
-    KeyCode rightKey = KeyCode.D;
-    KeyCode attackKey = KeyCode.J;
+    Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public float jumpSpeed = 5f;
+    float moveX;//左右移动输入
+    bool moveY;//跳跃输入
+    bool isOnFloor = true;//判断在空中还是在地上
+    KeyCode attackKey;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         renderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        attackKey = KeyCode.J;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(rightKey))
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetButtonDown("Jump");
+        MoveY();
+    }
+
+    void FixedUpdate()
+    {
+        
+        MoveX();
+        Attack();
+    }
+
+    void MoveX()
+    {
+        
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        if(moveX != 0)
         {
-            transform.Translate(moveSpeed, 0f, 0f);
-            renderer.flipX = false;
+            transform.localScale = new Vector3(moveX * 3, 3, 1);
             anim.SetBool("run", true);
-            Attack();
-        }
-        else if(Input.GetKey(leftKey))
-        {
-            transform.Translate(-moveSpeed, 0f, 0f);
-            renderer.flipX = true;
-            anim.SetBool("run", true);
-            Attack();
-        }
-        else if(Input.GetKey(upKey))
-        {
-            transform.Translate(0f, moveSpeed, 0f);
-            anim.SetBool("run", true);
-            Attack();
-        }
-        else if(Input.GetKey(downKey))
-        {
-            transform.Translate(0f, -moveSpeed, 0f);
-            anim.SetBool("run", true);
-            Attack();
         }
         else
         {
             anim.SetBool("run", false);
-            anim.SetBool("attack", false);
         }
-        Attack();
+        
+    }
+
+    void MoveY()
+    {
+        if(moveY && isOnFloor)
+        {
+            rb.velocity = Vector2.up * jumpSpeed;
+            isOnFloor = false;
+            anim.SetBool("jump", true);
+        }
     }
 
     void Attack()
@@ -62,6 +70,21 @@ public class Player : MonoBehaviour
         if(Input.GetKey(attackKey))
         {
             anim.SetBool("attack", true);
+        }
+        else
+        {
+            anim.SetBool("attack", false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("other.contacts[0].normal" + other.contacts[0].normal);
+        if(other.contacts[0].normal == Vector2.up)
+        {
+            Debug.Log("OnFloor");
+            isOnFloor = true;
+            anim.SetBool("jump", false);
         }
     }
 }
